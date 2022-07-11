cb10d295549ff8471d8f3dfcaff21f781af2f50a
Szymonxx1 <szymon_rys@onet.eu>
2022-07-11 20:03:39 +0200

    Poprawki logowanie i rejestracja

--- src/components/logIn.js
+++ src/components/logIn.js
@@ -1,15 +1,20 @@
 import React, { useState, useEffect } from "react";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase";
-import { Link as Link1 } from 'react-router-dom';
+import { Link as Link1, useNavigate } from 'react-router-dom';
 
 const LogIn = () => {
     const [logEmail, setLogEmail] = useState("");
     const [logPsw, setLogPsw] = useState("");
     const [a, setA] = useState("logIn_btns_login err");
+    const [b, setB] = useState("logIn_form_email");
+    const [c, setC] = useState("logIn_form_pswd")
     const [mailMsg, setMailMsg] = useState("");
     const [pswdMsg, setPswdMsg] = useState("");
     const emailRule = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{1,})$/i;
+    const [generalError, setGeneralError] = useState(null)
+
+    const navigate = useNavigate()
 
     useEffect(() => {
         if (emailRule.test(logEmail) && logPsw.length > 5) {
@@ -23,10 +28,13 @@ const LogIn = () => {
         if (logEmail.length > 0) {
             if (emailRule.test(logEmail) === false) {
                 setMailMsg('Niepoprawny format');
+                setB("logIn_form_email invalid");
             } else if (logEmail.length === 0) {
                 setMailMsg('To pole nie może być puste');
+                setB("logIn_form_email");;
             } else {
                 setMailMsg('');
+                setB("logIn_form_email");
             }
         }
     }, [logEmail]);
@@ -34,20 +42,27 @@ const LogIn = () => {
     useEffect(() => {
         if (logPsw.length > 0) {
             if (logPsw.length < 6) {
-                setPswdMsg('Hasło musi mieć minimum 6 znaków');
+                setPswdMsg('Podane hasło jest za krótkie!');
+                setC("logIn_form_pswd invalid");
             } else {
                 setPswdMsg('');
+                setC("logIn_form_pswd");
             }
         }
     }, [logPsw]);
 
 
-    const logUser = async () => {
+    const logUser = async (e) => {
+        e.preventDefault()
+        setGeneralError(null)
         try {
             const user = await signInWithEmailAndPassword(auth, logEmail, logPsw);
             console.log(user);
+            navigate("/")
         } catch (error) {
-            console.error(error.messege);
+            setGeneralError("Adres e-mail lub hasło są niepoprawne");
+            setLogEmail("");
+            setLogPsw("");
         }
     }
 
@@ -69,22 +84,26 @@ const LogIn = () => {
             <div className="logIn_box_form">
                 <h1>Zaloguj się</h1>
                 <span></span>
-                <form>
-                    <div className="logIn_form_email logIn_form">
-                        <label>Email</label>
-                        <input type="email" className="logIn_form_email" onChange={(e) => { setLogEmail(e.target.value) }} />
-                        <p className="mail_err">{mailMsg}</p>
+                <form onSubmit={logUser}>
+                    <div className="form">
+                    {generalError && <p className="log_error">{generalError}</p>}
+                        <div className="logIn_form_email logIn_form">
+                            <label>Email</label>
+                            <input type="email" className={b}  value={logEmail} onChange={(e) => { setLogEmail(e.target.value) }} />
+                            <p className="mail_err">{mailMsg}</p>
+                        </div>
+                        <div className="logIn_form_password logIn_form">
+                            <label>Hasło</label>
+                            <input type="password" className={c} value={logPsw} onChange={(e) => { setLogPsw(e.target.value) }} />
+                            <p className="psw_err">{pswdMsg}</p>
+                        </div>
                     </div>
-                    <div className="logIn_form_password logIn_form">
-                        <label>Hasło</label>
-                        <input type="password" className="logIn_form_pswd" onChange={(e) => { setLogPsw(e.target.value) }} />
-                        <p className="psw_err">{pswdMsg}</p>
+                    <div className="logIn_btns">
+                        <Link1 to='/rejestracja' className="logIn_btns_register">Załóż konto</Link1>
+                        <button className={a} type="submit">Zaloguj się</button>
                     </div>
                 </form>
-                <div className="logIn_btns">
-                    <Link1 to='/rejestracja' className="logIn_btns_register">Załóż konto</Link1>
-                    <Link1 to='/' className={a} onClick={logUser}>Zaloguj się</Link1>
-                </div>
+
             </div>
         </section>
     );
